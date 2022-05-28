4dd0c8f615055baf883c03119fd92b1e6ad9fe0b
omri rata <omri.rata@live.biu.ac.il>
2022-05-29 02:34:30 +0300

    get messages by GET request  to the api

--- src/components/ChatPage/Message/Messages.js
+++ src/components/ChatPage/Message/Messages.js
@@ -5,11 +5,34 @@ import Message from './Message';
 
 
 
-function Messages({ messages ,img}) {
-
-    const t = messages.map((i, key) => {
-        return <Message key={key} sent={i.sent} data={i.message} img={img} time={i.time && i.time} />
+function Messages({ messages, id }) {
+    const [mess, setMess] = useState([]);
+    const t = mess.map((i, key) => {
+        return <Message key={key} sent={i   .sent} data={i.content} /*img={img}*/ time={i.created} />
     })
+
+    useEffect(() => {
+        fetch("https://localhost:7191/api/contacts/"+id+"/messages", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': 'Bearer ' + localStorage.getItem("token")
+
+            }
+        }).then(response => response.json())
+            .then(res => {
+                console.log(res);
+                setMess(res);
+                console.log(res.status);
+                var status = res.status;
+                if (!status) {
+                    console.log("got contact on chatPage");
+                } else {
+                    console.log("not go the contacts on chatPage")
+                }
+            });
+    }, [id]);
 
     return (
         <div  className='mess'>
